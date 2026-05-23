af3077fe5bd90982307a5d4db95e412ad0c930ce
StefanoDisalvo21 <stefanodisalvo21@gmail.com>
2026-05-23 09:14:29 +0200

    feat: add selection sort & tests

--- include/algolib/sort.hpp
+++ include/algolib/sort.hpp
@@ -14,4 +14,20 @@ namespace sorting_algorithms{
             }
         }
     }
+    //end bubble sort
+
+    //selection sort O(n^2)
+    template<typename T>
+    void selection_sort(vector<T>&vc){
+        for(int i=0;i<vc.size()-1;++i){
+            int index_of_min_value = i;
+            for(int j=i+1;j<vc.size();++j){
+                if(vc[index_of_min_value]>vc[j]){
+                    index_of_min_value = j;
+                }
+            }
+            swap(vc[i],vc[index_of_min_value]);
+        }
+    }
+    //end selection sort
 }
