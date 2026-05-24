6b55cd28e3bd0e3675e82f5010c4982d9bcc6248
StefanoDisalvo21 <stefanodisalvo21@gmail.com>
2026-05-24 12:20:42 +0200

    feat: quicksort

--- include/algolib/sort.hpp
+++ include/algolib/sort.hpp
@@ -30,4 +30,31 @@ namespace sorting_algorithms{
         }
     }
     //end selection sort
+
+    //quicksort
+    template<typename T>
+    int partitioning(vector<T>& vc, int start, int end){
+        int pivot = v[end];
+        int i= start-1;
+        for(int j=0;j<end;++j){
+            if(vc[j]<=pivot){
+                ++i;
+                swap(vc[i],vc[j]);
+            }
+        }
+
+        return (++i);
+    }//end partitioning function
+
+    template<typename T>
+    void quicksort(vector<T>& vc, int start, int end){
+        if(start<end){
+            int partition = partitioning(vc,start,end);
+
+            quicksort(vc,start,part-1);
+            quicksort(vc,part+1,end);
+        }
+    }
+
+    //end quicksort
 }
