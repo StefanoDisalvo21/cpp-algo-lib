9a52e5bb069f32ed92c0860f17e15d3d1096359c
StefanoDisalvo21 <stefanodisalvo21@gmail.com>
2026-05-25 09:44:01 +0200

    feat: add is_sorted control function

---
+++ include/algolib/helpers_function.hpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#pragma once
+using namespace std;
+
+
+namespace helpers_functions{
+    
+    //is sorted array checks
+    template<typename T>
+    bool is_sorted(vector<T>& vc){
+        for(size_t i=0;i<vc.size()-1;++i){
+            if(vc[i]>v[i+1]){
+                return false
+            }
+        }
+        return true;
+    }
+    //end checking function
+}
